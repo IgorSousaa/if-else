ace2b0b785057a49ecded798f68278f6c30f604b
IgorSousaa <igoralvesnasc2@gmail.com>
2022-06-14 18:22:44 -0300

    Trabalho de If/Else

---
+++ script.js
@@ -0,0 +1,27 @@
+let idade = 18;
+if(idade){
+    console.log("Tem 18 anos, então pode entrar!")
+}else{
+    console.log("Apenas maiores de 18 anos podem entrar, desculpe.")
+}
+
+let maiorIdade = true;
+if(maiorIdade){
+    console.log("É maior de Idade");
+}else{
+    console.log("Não é maior de Idade");
+}
+
+let mes = "Janeiro";
+if((mes === "Janeiro") || (mes === "Dezembro")){
+    console.log("Faz aniversário em Janeiro");
+}else{
+    console.log("Faz aniversário em Dezembro");
+}
+
+let primeiraLetra = true;
+if(primeiraLetra){
+    console.log("Apenas nomes que começam com R");
+}else{
+    console.log("Infelizmente seu nome não começa com R.");
+}
